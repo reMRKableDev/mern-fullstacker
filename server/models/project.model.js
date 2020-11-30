53fd99d1f399fda1d73a9b1a92c17e23a0964881
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-30 16:41:58 +0100

    :card_file_box: Models finished

---
+++ server/models/project.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const projectSchema = new Schema({
+  title: String,
+  description: String,
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  // owner will be added later on
+});
+
+module.exports = model("Project", projectSchema);
