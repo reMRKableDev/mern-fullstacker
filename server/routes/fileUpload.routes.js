31fe0e7c5983e81d27ade3707f5f7a6a3b9d3f85
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-03 09:30:47 +0100

    :art: fixes

--- server/routes/fileUpload.routes.js
+++ server/routes/fileUpload.routes.js
@@ -6,12 +6,7 @@ const fileUploader = require("../configs/cloudinary.config");
 /* POST - upload images   */
 router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
   //console.log("file is: ", req.file);
-
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-
+  
   // Get the path from req.file and pass it along
   const { path } = req.file;
 
