c3ee3e90a5b3354f0c1f5bde70d1b081f7ceb61e
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-03 22:53:40 +0100

    :fire: removed unused axios import

--- client/src/components/Projects/ProjectList.js
+++ client/src/components/Projects/ProjectList.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 
-import ProjectService from "../../services/project-service";
-
 import AddProjectForm from "./Forms/AddProjectForm";
+import ProjectService from "../../services/project-service";
 
 const ProjectList = () => {
   const [listOfProjects, setListOfProjects] = useState([]);
